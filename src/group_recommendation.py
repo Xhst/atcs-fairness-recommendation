119db486f66fcc7d36014b7ed68b3bce1ebf3eed
Mattia <82611659+Xhst@users.noreply.github.com>
2024-03-15 17:09:14 +0100

    feat: Avarage aggregation method
    Co-Authored-By: CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>

--- src/group_recommendation.py
+++ src/group_recommendation.py
@@ -4,7 +4,7 @@ import math
 
 class GroupRecommendation:
 
-    def aggregate_users_recommendations(users: set[int], n: int = 10, neighbor_size: int = 50):
+    def aggregate_users_recommendations(users: set[int], n: int = 10, neighbor_size: int = 50) -> dict[int, list[float]]:
 
         aggregate_recommendations: dict[int, list[float]] = {}
 
@@ -18,3 +18,15 @@ class GroupRecommendation:
                     aggregate_recommendations.get(movie).append(predicted_rating)
 
         return aggregate_recommendations
+    
+
+    def avarage_aggregation(users: set[int], n: int = 10) -> list[(int, float)]:
+        users_rec = GroupRecommendation.aggregate_users_recommendations(users)
+
+        avg_rec: list[(int, float)] = []
+
+        for movie, predicted_ratings in users_rec.items():
+            avarage = sum(predicted_ratings) / len(predicted_ratings)
+            avg_rec.append((movie, avarage))
+
+        return avg_rec[:n]
